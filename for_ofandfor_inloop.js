8a24ff44b271e135130aa38418e4297e22c3658d
mujahidulsiyam <mujahidul.siyam@gmail.com>
2026-08-25 11:35:32 +0600

    modernjs

---
+++ for_ofandfor_inloop.js
@@ -0,0 +1,19 @@
+// let techstack = ["js","ts","node","react","psql"];
+
+// for(let tech of techstack){
+//     console.log(tech);
+// }
+// // for(let tech in techstack){
+// //     console.log(tech);
+// // }
+
+
+let obj = {
+    name :"Mujahidul",
+    age:25,
+    address :"Dhaka",
+}
+
+for(let ele in obj){
+    console.log(ele);
+}

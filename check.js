b565143ef5e306ffcbb34b1247858a8115bdb401
mujahidulsiyam <mujahidul.siyam@gmail.com>
2026-08-27 20:21:56 +0600

    export import

---
+++ check.js
@@ -0,0 +1,7 @@
+import add from "./math.js";
+import {subs,pi} from "./math.js";
+
+console.log(add(5,3));
+
+console.log(subs(70,9));
+console.log(pi);
---
+++ math.js
@@ -0,0 +1,17 @@
+// export import 
+function add(a,b){
+    return a+b
+}
+
+export function subs(a,b){
+    if(a>b){
+        return a-b;
+    }
+    else{
+        return b-a;
+    }
+}
+
+export const pi=3.1416;
+
+export default add;

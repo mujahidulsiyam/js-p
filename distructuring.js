4e423000d17d759c909a45458d150422e16ee92f
mujahidulsiyam <mujahidul.siyam@gmail.com>
2026-08-27 13:29:20 +0600

    distructing_and_note

--- distructuring.js
+++ distructuring.js
@@ -9,16 +9,43 @@
 // // new 
 // const {age,name,address} = student
 
-// console.log(name);
+// // console.log(name);
 
-const student1= {
+// const student1= {
+//     name:"Mujahidul",
+//     age:20,
+//     address:{
+//         city:"Dhaka",
+//         zip:1221,
+//     },
+// }
+
+// const {name:stdName,address:{city,zip}}=student1;
+// console.log(stdName,age,city);b
+
+
+// array 
+
+// const arr = ["red","green","blue"]
+
+// const [,,Third]=arr;
+
+// console.log(Third);
+
+
+// Nested array 
+
+const student2= {
     name:"Mujahidul",
     age:20,
     address:{
         city:"Dhaka",
         zip:1221,
     },
+    hobbies :["Gardening","Painting"],
 }
 
-const {name,age,address:{city,zip}}=student1;
-console.log(name,age,city);
+const {name,address:{city,zip},hobbies:[first],} =student2
+
+
+console.log(first);
